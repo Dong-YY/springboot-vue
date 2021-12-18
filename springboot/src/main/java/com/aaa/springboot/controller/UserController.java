5214a1b9f47ee5eb58e4cc8b8a78b061a46e56ac
dong <1584181080@qq.com>
2021-12-18 13:34:56 +0800

    @CrossOrigin注解注释

--- springboot/src/main/java/com/aaa/springboot/controller/UserController.java
+++ springboot/src/main/java/com/aaa/springboot/controller/UserController.java
@@ -9,7 +9,7 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
-@CrossOrigin
+@CrossOrigin  //跨域
 @RestController
 @RequestMapping("/user")
 public class UserController {
